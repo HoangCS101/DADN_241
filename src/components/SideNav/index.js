0e85db369b788f87163cb6492298d07181da90be
tiendunglaptrinh <dung.takhmtk21@hcmut.edu.vn>
2024-12-10 22:19:01 +0700

    feat: layout

--- src/components/SideNav/index.js
+++ src/components/SideNav/index.js
@@ -12,69 +12,82 @@ import styles from "./SideNav.component.scss";
 const cx = classNames.bind(styles);
 function SideNav() {
   return (
-    <div className={cx("wrapper-sidenav")}>
-      <div className={cx("sidenav-intro")}>
-        <div className={cx("key-title")}>A</div>
-        <div className={cx("sidenav-title")}>SMART HOUSE - DEVICES</div>
-      </div>
-      <div className={cx("sidenav-options")}>
-        <Link to="/">
-          <div className={cx("sidenav-item")}>
-            <div className={cx("sidenav-icon")}>
-              <FontAwesomeIcon
-                style={{ color: "#efbb35", fontSize: "25px" }}
-                icon={faChartColumn}
+    <div>
+      {/* Main Sidebar Container */}
+      <aside className="main-sidebar sidebar-dark-primary elevation-4">
+        {/* Brand Logo */}
+        <a href="index3.html" className="brand-link">
+          <img
+            src="dist/img/AdminLTELogo.png"
+            alt="AdminLTE Logo"
+            className="brand-image img-circle elevation-3"
+            style={{ opacity: ".8" }}
+          />
+          <span className="brand-text font-weight-light">AdminLTE 3</span>
+        </a>
+        {/* Sidebar */}
+        <div className="sidebar">
+          {/* Sidebar user panel (optional) */}
+          <div className="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div className="image">
+              <img
+                src="dist/img/user2-160x160.jpg"
+                className="img-circle elevation-2"
+                alt="User Image"
               />
             </div>
-            <div className={cx("sidenav-content")}>Thống kê sử dụng</div>
-          </div>
-        </Link>
-        <Link to="/control-device">
-          <div className={cx("sidenav-item")}>
-            <div className={cx("sidenav-icon")}>
-              <FontAwesomeIcon
-                style={{ color: "#efbb35", fontSize: "25px" }}
-                icon={faGear}
-              />
+            <div className="info">
+              <a href="#" className="d-block">
+                Alexander Pierce
+              </a>
             </div>
-            <div className={cx("sidenav-content")}>Quản lý thiết bị</div>
           </div>
-        </Link>
-
-        {/* <Link to="/">
-          <div className={cx("sidenav-item")}>
-            <div className={cx("sidenav-icon")}>
-              <FontAwesomeIcon
-                style={{ color: "#efbb35", fontSize: "25px" }}
-                icon={faTriangleExclamation}
+          {/* SidebarSearch Form */}
+          <div className="form-inline">
+            <div className="input-group" data-widget="sidebar-search">
+              <input
+                className="form-control form-control-sidebar"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
               />
+              <div className="input-group-append">
+                <button className="btn btn-sidebar">
+                  <i className="fas fa-search fa-fw" />
+                </button>
+              </div>
             </div>
-            <div className={cx("sidenav-content")}>Cảnh báo</div>
           </div>
-        </Link>
-        <Link to="/profile">
-          <div className={cx("sidenav-item")}>
-            <div className={cx("sidenav-icon")}>
-              <FontAwesomeIcon
-                style={{ color: "#efbb35", fontSize: "25px" }}
-                icon={faUser}
-              />
-            </div>
-            <div className={cx("sidenav-content")}>Thông tin người dùng</div>
-          </div>
-        </Link>
-        <Link to="/notification">
-          <div className={cx("sidenav-item")}>
-            <div className={cx("sidenav-icon")}>
-              <FontAwesomeIcon
-                style={{ color: "#efbb35", fontSize: "25px" }}
-                icon={faBell}
-              />
-            </div>
-            <div className={cx("sidenav-content")}>Thông báo hệ thống</div>
-          </div>
-        </Link> */}
-      </div>
+          {/* Sidebar Menu */}
+          <nav className="mt-2">
+            <ul
+              className="nav nav-pills nav-sidebar flex-column"
+              data-widget="treeview"
+              role="menu"
+              data-accordion="false"
+            >
+              {/* Add icons to the links using the .nav-icon class
+         with font-awesome or any other icon font library */}
+              <li className="nav-item">
+                {/* <a href="#" className="nav-link active"> */}
+                <Link to="/" className="nav-link">
+                  <i className="nav-icon far fa-image" />
+                  <p>Thống kê sử dụng</p>
+                </Link>
+                {/* </a> */}
+              </li>
+              <li className="nav-item">
+                <Link to="/control-device" className="nav-link">
+                  <i className="nav-icon far fa-image" />
+                  <p>Điều khiển thiết bị</p>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          {/* /.sidebar-menu */}
+        </div>
+        {/* /.sidebar */}
+      </aside>
     </div>
   );
 }
